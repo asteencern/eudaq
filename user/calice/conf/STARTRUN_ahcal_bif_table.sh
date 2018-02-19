f856f114bb7f1d05f0ce6b5193d8c4bc05fb8c08
calice <calice@localhost.localdomain>
2018-02-19 08:19:08 +0100

    added config script for 2018-02-18

--- user/calice/conf/STARTRUN_ahcal_bif_table.sh
+++ user/calice/conf/STARTRUN_ahcal_bif_table.sh
@@ -32,7 +32,7 @@ xterm -r -sb -sl 100000 -geometry 160x24 -T "BXID collector ahcalbif" -e 'bin/eu
 #xterm -sb -sl 1000000 -geometry 160x30 -T "Hodoscope 2" -e 'bin/euCliProducer -n CaliceEasirocProducer -t hodoscope2 |tee -a data/logs/hodoscope2.log ; read'&
 # sleep 1
 xterm -r -sb -sl 100000 -geometry 160x24 -T "AHCAL" -e 'bin/euCliProducer -n AHCALProducer -t AHCAL1 |tee -a data/logs/ahcal.log ; read'&
-xterm -r -sb -sl 100000 -geometry 160x24 -T "BIF" -e 'bin/euCliProducer -n caliceahcalbifProducer -t BIF1 |tee -a data/logs/ahcal.log ; read'&
+xterm -r -sb -sl 100000 -geometry 160x24 -T "BIF" -e 'bin/euCliProducer -n caliceahcalbifProducer -t BIF1 |tee -a data/logs/bif.log ; read'&
 xterm -r -sb -sl 10000 -geometry 160x24 -T "Table DESY" -e 'bin/euCliProducer -n DesyTableProducer -t desytable1 |tee -a data/logs/desytable.log ;read'& 
 #################  Online Monitor #################
 echo "starting online monitor"
