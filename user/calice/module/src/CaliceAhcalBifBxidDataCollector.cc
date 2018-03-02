c954ecc0df7aa742aebadfe2098a877c47552b3f
Jiri Kvasnicka <jiri.kvasnicka@desy.de>
2018-03-02 17:22:14 +0100

    data collector works with hodoscope

--- user/calice/module/src/CaliceAhcalBifBxidDataCollector.cc
+++ user/calice/module/src/CaliceAhcalBifBxidDataCollector.cc
@@ -19,7 +19,8 @@ class CaliceAhcalBifBxidDataCollector: public eudaq::DataCollector {
       void AddEvent_TimeStamp(uint32_t id, eudaq::EventSPC ev);
       void BuildEvent_bxid();
 
-      //to be edited according to the name of the processes
+      std::mutex m_mutex;
+      //to be edited according to the name of the processes. TODO implement as parameter to the configuration
       std::string mc_name_ahcal = "AHCAL1";
       std::string mc_name_bif = "BIF1";
       std::string mc_name_hodoscope1 = "Hodoscope1";
@@ -50,9 +51,11 @@ class CaliceAhcalBifBxidDataCollector: public eudaq::DataCollector {
       //final test whether the event will be thrown away if a mandatory subevents are not present
       bool m_evt_mandatory_ahcal = true;
       bool m_evt_mandatory_bif = true;
-      bool m_evt_mandatory_hodoscope1 = false;
-      bool m_evt_mandatory_hodoscope2 = false;
+      bool m_evt_mandatory_hodoscope1 = true;
+      bool m_evt_mandatory_hodoscope2 = true;
       bool m_evt_mandatory_desytable = false;
+
+      int m_thrown_incomplete = 0; //how many event were thrown away because there were incomplete
 
       std::chrono::time_point<std::chrono::system_clock> lastprinttime;
       eudaq::EventSPC m_ev_last_cal;
@@ -61,7 +64,6 @@ class CaliceAhcalBifBxidDataCollector: public eudaq::DataCollector {
       uint64_t m_ts_end_last_bif;
       bool m_offset_ahcal2bif_done;
       int64_t m_ts_offset_ahcal2bif;
-
       uint32_t m_ev_n;
 };
 
@@ -87,6 +89,7 @@ void CaliceAhcalBifBxidDataCollector::DoStartRun() {
    m_offset_ahcal2bif_done = false;
    m_ts_offset_ahcal2bif = 0;
    m_ev_n = 0;
+   m_thrown_incomplete = 0;
 }
 
 void CaliceAhcalBifBxidDataCollector::DoConfigure() {
@@ -113,7 +116,7 @@ void CaliceAhcalBifBxidDataCollector::DoConnect(eudaq::ConnectionSPC idx) {
       m_active_hodoscope1 = true;
       recognizedProducer = true;
    }
-   if (idx->GetName() == mc_name_hodoscope1) {
+   if (idx->GetName() == mc_name_hodoscope2) {
       m_active_hodoscope2 = true;
       recognizedProducer = true;
    }
@@ -134,6 +137,7 @@ void CaliceAhcalBifBxidDataCollector::DoDisconnect(eudaq::ConnectionSPC idx) {
    if (idx->GetName() == mc_name_hodoscope1) m_active_hodoscope1 = false;
    if (idx->GetName() == mc_name_hodoscope2) m_active_hodoscope2 = false;
    if (idx->GetName() == mc_name_desytable) m_active_desytable = false;
+   std::cout << m_thrown_incomplete << " incomplete events thrown away" << std::endl;
 }
 
 void CaliceAhcalBifBxidDataCollector::DoReceive(eudaq::ConnectionSPC idx, eudaq::EventSP ev) {
@@ -197,7 +201,7 @@ inline void CaliceAhcalBifBxidDataCollector::BuildEvent_bxid() {
 //         // std::cout<<"offset "<< m_ts_offset_cal2bif <<std::endl;
 //      }
 //   }
-
+   std::lock_guard<std::mutex> lock(m_mutex);//only 1 process should be allowed to make events
    while (true) {
       if (std::chrono::system_clock::now() - lastprinttime > std::chrono::milliseconds(1000)) {
          lastprinttime = std::chrono::system_clock::now();
@@ -239,6 +243,7 @@ inline void CaliceAhcalBifBxidDataCollector::BuildEvent_bxid() {
             m_que_ahcal.pop_front();
             continue;
          }
+         roc_ahcal += m_roc_offset_ahcal;
       }
 
       if (m_que_bif.empty()) {
@@ -253,6 +258,7 @@ inline void CaliceAhcalBifBxidDataCollector::BuildEvent_bxid() {
             m_que_bif.pop_front();
             continue;
          }
+         roc_bif += m_roc_offset_bif;
          if (bxid_bif < -100) { //cca 400 us, which can be spend on the power pulsing
             std::cout << "DEBUG: event " + std::to_string(m_que_bif.front()->GetEventN()) + " has too low bxid ("
                   + std::to_string(bxid_bif) + ") in run " + std::to_string(m_que_bif.front()->GetRunNumber()) << std::endl;
@@ -261,21 +267,58 @@ inline void CaliceAhcalBifBxidDataCollector::BuildEvent_bxid() {
          }
       }
 
+      if (m_que_hodoscope1.empty()) {
+         if (m_active_hodoscope1) return;
+      } else {
+         roc_hodoscope1 = m_que_hodoscope1.front()->GetTag("ROC", mc_roc_invalid);
+         bxid_bif = m_que_hodoscope1.front()->GetTag("BXID", mc_bxid_invalid);
+         if ((roc_hodoscope1 == mc_roc_invalid) || (bxid_hodoscope1 == mc_bxid_invalid)) {
+            EUDAQ_WARN_STREAMOUT("event " + std::to_string(m_que_hodoscope1.front()->GetEventN()) +
+                  " without ROC(" + std::to_string(roc_hodoscope1) +
+                  ") or BXID(" + std::to_string(bxid_hodoscope1) +
+                  ") in run " + std::to_string(m_que_hodoscope1.front()->GetRunNumber()), std::cout, std::cerr);
+            m_que_hodoscope1.pop_front();
+            continue;
+         }
+         roc_hodoscope1 += m_roc_offset_hodoscope1;
+      }
+
+      if (m_que_hodoscope2.empty()) {
+         if (m_active_hodoscope2) return;
+      } else {
+         roc_hodoscope2 = m_que_hodoscope2.front()->GetTag("ROC", mc_roc_invalid);
+         bxid_bif = m_que_hodoscope2.front()->GetTag("BXID", mc_bxid_invalid);
+         if ((roc_hodoscope2 == mc_roc_invalid) || (bxid_hodoscope2 == mc_bxid_invalid)) {
+            EUDAQ_WARN_STREAMOUT("event " + std::to_string(m_que_hodoscope2.front()->GetEventN()) +
+                  " without ROC(" + std::to_string(roc_hodoscope2) +
+                  ") or BXID(" + std::to_string(bxid_hodoscope2) +
+                  ") in run " + std::to_string(m_que_hodoscope2.front()->GetRunNumber()), std::cout, std::cerr);
+            m_que_hodoscope2.pop_front();
+            continue;
+         }
+         roc_hodoscope2 += m_roc_offset_hodoscope2;
+      }
+
       // at this point all producers have something in the buffer (if possible)
       bool present_ahcal = false, present_bif = false, present_hodoscope1 = false, present_hodoscope2 = false, present_desytable = false; //whether event is present in the merged event
 
       int processedRoc = mc_roc_invalid;
       if (roc_ahcal < processedRoc) processedRoc = roc_ahcal;
       if (roc_bif < processedRoc) processedRoc = roc_bif;
-      //TODO hodoscopes
+      if (roc_hodoscope1 < processedRoc) processedRoc = roc_hodoscope1;
+      if (roc_hodoscope2 < processedRoc) processedRoc = roc_hodoscope2;
 
       int processedBxid = mc_bxid_invalid;
       if ((roc_ahcal == processedRoc) && (bxid_ahcal < processedBxid)) processedBxid = bxid_ahcal;
       if ((roc_bif == processedRoc) && (bxid_bif < processedBxid)) processedBxid = bxid_bif;
-      //todo hodoscopes
+      if ((roc_hodoscope1 == processedRoc) && (bxid_hodoscope1 < processedBxid)) processedBxid = bxid_hodoscope1;
+      if ((roc_hodoscope2 == processedRoc) && (bxid_hodoscope2 < processedBxid)) processedBxid = bxid_hodoscope2;
 
-      //std::cout << "Trying to put together event with ROC=" << processedRoc << " BXID=" << processedBxid;
-      //std::cout << "\tAHCALROC=" << roc_ahcal << ",AHCALBXID=" << bxid_ahcal << ",BIFROC=" << roc_bif << ",BIFBXID=" << bxid_bif << std::endl;
+      std::cout << "Trying to put together event with ROC=" << processedRoc << " BXID=" << processedBxid;
+      std::cout << "\tAHCALROC=" << roc_ahcal << ",AHCALBXID=" << bxid_ahcal << ",BIFROC=" << roc_bif << ",BIFBXID=" << bxid_bif;
+      std::cout << "\tH1ROC=" << roc_hodoscope1 << ",H1BXID=" << bxid_hodoscope1;
+      std::cout << "\tH2ROC=" << roc_hodoscope2 << ",H2BXID=" << bxid_hodoscope2 << std::endl;
+
       auto ev_sync = eudaq::Event::MakeUnique("CaliceBxid");
       ev_sync->SetFlagPacket();
       if ((roc_ahcal == processedRoc) && (bxid_ahcal == processedBxid)) {
@@ -294,15 +337,33 @@ inline void CaliceAhcalBifBxidDataCollector::BuildEvent_bxid() {
          }
       }
 
+      if ((roc_hodoscope1 == processedRoc) && (bxid_hodoscope1 == processedBxid)) {
+         if (!m_que_hodoscope1.empty()) {
+            ev_sync->AddSubEvent(std::move(m_que_hodoscope1.front()));
+            m_que_hodoscope1.pop_front();
+            present_hodoscope1 = true;
+         }
+      }
+
+      if ((roc_hodoscope2 == processedRoc) && (bxid_hodoscope2 == processedBxid)) {
+         if (!m_que_hodoscope2.empty()) {
+            ev_sync->AddSubEvent(std::move(m_que_hodoscope2.front()));
+            m_que_hodoscope2.pop_front();
+            present_hodoscope2 = true;
+         }
+      }
+
       if (!m_que_desytable.empty()) {
          ev_sync->AddSubEvent(std::move(m_que_desytable.front()));
          m_que_desytable.pop_front();
          present_desytable = true;
       }
-      if (m_evt_mandatory_ahcal && (!present_ahcal)) continue; //throw awway incomplete event
-      if (m_evt_mandatory_bif && (!present_bif)) continue; //throw awway incomplete event
-      if (m_evt_mandatory_hodoscope1 && (!present_hodoscope1)) continue; //throw awway incomplete event
-      if (m_evt_mandatory_hodoscope2 && (!present_hodoscope2)) continue; //throw awway incomplete event
+      m_thrown_incomplete += 1; //increase in case the loop is exit in following lines
+      if (m_evt_mandatory_ahcal && (!present_ahcal) && (!m_active_ahcal)) continue; //throw awway incomplete event
+      if (m_evt_mandatory_bif && (!present_bif) && (!m_active_bif)) continue; //throw awway incomplete event
+      if (m_evt_mandatory_hodoscope1 && (!present_hodoscope1) && (!m_active_hodoscope1)) continue; //throw awway incomplete event
+      if (m_evt_mandatory_hodoscope2 && (!present_hodoscope2) && (!m_active_hodoscope2)) continue; //throw awway incomplete event
+      m_thrown_incomplete -= 1; //and decrease back.
       ev_sync->SetEventN(m_ev_n++);
 //      ev_sync->Print(std::cout);
       WriteEvent(std::move(ev_sync));
